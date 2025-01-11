b5076edd82109b5a096b36be0516e2066aebb39e
Axel Velasquez <yoloax11@gmail.com>
2025-01-11 12:48:10 -0800

    movie model modifications to link to users

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -1,38 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const movieSchema = new Schema({
-  title: {
-    type: String,
-    required: true
+const movieSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    releaseYear: {
+      type: Number,
+      required: true,
+    },
+    genres: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Genre",
+        required: true,
+      },
+    ],
+    watched: {
+      type: Boolean,
+      default: false,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      default: 1,
+    },
+    poster: {
+      type: String,
+      required: true,
+    },
+    user_id: {
+      type: String,
+      required: true,
+    },
   },
-  releaseYear: {
-    type: Number,
-    required: true
-  },
-  genres: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Genre', 
-    required: true
-  }],
-  watched: {
-    type: Boolean,
-    default: false
-  },
-  rating: {
-    type: Number,
-    min: 1,
-    max: 5,
-    default:1
-  },
-  poster:{
-    type:String, 
-    required: true,
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true  
-});
+);
 
-const Movie = mongoose.model('Movie', movieSchema);
+const Movie = mongoose.model("Movie", movieSchema);
 
 module.exports = Movie;
