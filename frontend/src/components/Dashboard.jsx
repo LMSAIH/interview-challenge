1575a5b4385b99432d8b0d5e1ffbf79e6162b415
Axel Velasquez <yoloax11@gmail.com>
2025-01-10 22:06:21 -0800

    creating dashboard and other elements

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,38 @@
+import { Menu03Icon } from "hugeicons-react";
+import { Sun01Icon } from "hugeicons-react";
+import { Film01Icon } from "hugeicons-react";
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            <div className="top">
+
+                <div className="leftSectionTop">
+                    <div className="expandIcon">
+                        <Menu03Icon />
+                    </div>
+                    <div className="titleAndLogoContainer" >
+                        <h1> Watchlist </h1>
+                        <Film01Icon color ="white"/> 
+                    </div>
+                </div>
+
+
+                <div className="darkLightContainer">
+                    <Sun01Icon />
+                </div>
+            </div>
+
+            <nav>
+                <div className="navContainer">
+                    <p> hey </p>
+                </div>
+            </nav>
+
+        </div>
+    );
+}
+
+export default Dashboard;
+
+
